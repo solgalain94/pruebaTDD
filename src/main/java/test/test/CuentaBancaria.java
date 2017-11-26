27e9573aef913e5eaa6bf163794314afd4871401
Cintia Galain <solgalain@gmail.com>
2017-11-26 19:59:53 -0300

    Test agregaSaldo Verde

--- src/main/java/test/test/CuentaBancaria.java
+++ src/main/java/test/test/CuentaBancaria.java
@@ -1,8 +1,12 @@
 package test.test;
 
 public class CuentaBancaria {
+	private int saldo=0;
 	public int getSaldo() {
-		return 0;
+		return this.saldo;
+	}
+	public void agregarSaldo(int valor) {
+		this.saldo=this.saldo+valor;
 	}
 
 }
